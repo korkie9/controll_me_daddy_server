9cba708677300e30da61dea7ae4966f6c1daf87f
Justin Korkie <justinjkorkie@gmail.com>
2025-03-21 22:58:42 +0200

    feat(main): add uinput

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module controll-me-daddy
 
 go 1.24.1
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/bendahl/uinput v1.7.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/bendahl/uinput"
 )
 
-// Upgrader is used to upgrade HTTP connections to WebSocket connections.
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -14,23 +17,28 @@ var upgrader = websocket.Upgrader{
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	// Upgrade the HTTP connection to a WebSocket connection
+	joystick, err := uinput.CreateGamepad("/dev/uinput", []byte("Virtual Joystick"), 0x1234, 0x5678)
+	if err != nil {
+		log.Fatalf("Failed to create virtual joystick: %v", err)
+	}
+	defer joystick.Close()
+
+	joystick.ButtonDown(304) // Max right
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Error upgrading:", err)
 		return
 	}
 	defer conn.Close()
-	// Listen for incoming messages
+
 	for {
-		// Read message from the client
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println("Error reading message:", err)
 			break
 		}
 		fmt.Printf("Received: %s\\n", message)
-		// Echo the message back to the client
 		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
 			fmt.Println("Error writing message:", err)
 			break
